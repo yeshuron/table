d43ae6e52bf0d08ead44b8a62811c07f0070e31e
yeshuron <jarrod.wolkowitz@gmail.com>
2019-01-07 09:52:36 -0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+  $("#accessStatus").click(function(){
+    $(".hideMeAcc").toggle();
+  });
+});
+
+
+$(document).ready(function(){
+  $("#fbk").click(function(){
+    $(".hideMeFb").toggle();
+  });
+});
+
+
+
+$(document).ready(function(){
+  $("#Mnu1").hover(function(){
+    $(".hideMeB").toggle();
+  });
+});
+
+
