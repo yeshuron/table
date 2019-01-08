a68ac3b9d2c5135e810a928ccb0bd70703e7e353
yeshuron <jarrod.wolkowitz@gmail.com>
2019-01-07 19:11:30 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,57 @@
 
-$(document).ready(function(){
   $("#accessStatus").click(function(){
     $(".hideMeAcc").toggle();
   });
-});
 
-
-$(document).ready(function(){
   $("#fbk").click(function(){
     $(".hideMeFb").toggle();
   });
-});
 
-
-
-$(document).ready(function(){
   $("#Mnu1").hover(function(){
     $(".hideMeB").toggle();
   });
-});
 
 
+
+var table = document.getElementById("myTable");
+var tr = table.getElementsByTagName("tr");
+
+function myFunction() {
+  var input = document.getElementById("myInput");
+  var filter = input.value.toUpperCase();
+  showMatchingCells(filter)
+}
+
+function clickFunction(event) {
+   var filter = event.target.textContent.toUpperCase();
+   showMatchingCells(filter);
+}
+[].slice.call(document.getElementsByClassName("filter-button"), 0).forEach(function(element) {
+  element.onclick = clickFunction;
+})
+document.getElementById('clear-button').onclick = function() { showMatchingCells('');     
+  $(".hideMeFb").hide();
+    $(".hideMeAcc").hide();
+  } 
+
+function showMatchingCells(filter) {
+    var txtValue, i, td;
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByClassName("meta")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function poppy() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function clickFunction(event) {
